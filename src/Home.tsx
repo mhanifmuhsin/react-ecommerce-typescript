baf8c98ba311211e5d0043b598079d4ecd4bb3b8
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2021-04-24 05:55:37 +0700

    clear code

--- src/Home.tsx
+++ src/Home.tsx
@@ -88,13 +88,12 @@ const Home: FC = () => {
                                             <li className="list-group-item"><strong>Category</strong> : {product.category}</li>
                                             <li className="list-group-item"><strong>Price</strong> : $ {product.price}</li>
                                         </ul>
-
                                         <button type="button" className="btn btn-secondary btn-sm m-2" data-toggle="modal" data-target="#detail" onClick={() => findById(product.id)} >
                                             Details
-                            </button>
+                                        </button>
                                         <button type="button" className="btn btn-primary btn-sm m-2" onClick={() => handleCart(product.name, product.price)}>
                                             Add to cart
-                            </button>
+                                        </button>
                                     </div>
                                 </div>
                             )
@@ -104,8 +103,6 @@ const Home: FC = () => {
                             Category Not found
                         </div>
                     )}
-
-
 
                     {/* modal detail */}
                     <div className="modal fade" id="detail" tab-index="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
